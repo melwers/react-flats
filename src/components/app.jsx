9c3e68433a797958d08188f0d32fc26ad30e7d1f
Marike Elwers <m.e.elwers@gmail.com>
2021-03-15 20:23:36 +0100

    completed app methods

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 
+import flats from '../../data/flats'
 import FlatList from './flat_list';
-
+import Marker from './marker';
 
 class App extends Component {
   constructor(props) {
@@ -12,14 +14,33 @@ class App extends Component {
     }
   };
 
+  center() {
+    return {
+      lat: this.state.selectedFlat.lat,
+      lng: this.state.selectedFlat.lng
+    };
+  }
+
+  selectFlat = (index) => {
+    this.setState({ selectedFlat: flats[index] });
+  }
+
+
 
   render() {
     return (
       <div> 
-        <FlatList />
-      </div>
+        <FlatList 
+          flats={this.state.flats}
+          selectedFlat={this.state.selectedFlat}
+          selectFlat={this.selectFlat}
+        />
       <div className="map-container"> 
+        <GoogleMapReact defaultCenter={this.center()} defaultZoom={12}>
+              <Marker lat={this.state.selectedFlat.lat} lng={this.state.selectedFlat.lng} />
+            </GoogleMapReact>
       </div>
+    </div>
     );
   }
 }
