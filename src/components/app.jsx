1eb831eb2edfa367b14b042dc9a684216aed8d79
Marike Elwers <m.e.elwers@gmail.com>
2021-03-15 19:55:49 +0100

    added basic components of the app

---
+++ src/components/app.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+import FlatList from './flat_list';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedFlat: flats[0],
+      flats
+    }
+  };
+
+
+  render() {
+    return (
+      <div> 
+        <FlatList />
+      </div>
+      <div className = "flat-container"> 
+      </div>
+    );
+  }
+}
+
+export default App;
